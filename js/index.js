f7b51675a63ab86d5c794480ae0d5b590e72247b
irene villanueva <irens.vm@gmail.com>
2020-04-26 13:04:06 +0200

    notshowincanvas

--- js/index.js
+++ js/index.js
@@ -1,65 +1,81 @@
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
 
+window.onload = () => {
+  document.getElementById('start-button').onclick = () => {
+    startGame();
+};
 
+function startGame() {
+  road.inicializar();
+  road.mostrar();
+  coche.inicializar();
+  coche.mostrar();
+  
+  
+};  
+ console.log(startGame);
+  
+const road = {
+  
+    img: null,
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    
+    inicializar: function() {
+      this.img= new Image();
+      this.img.src = "./images/road.png";
+      this.img.onload = this.mostrar;
+      this.widht = canvas.widht;
+      this.height = canvas.height;
+  
+  
+    },
+  
+    mostrar: function() {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  
+    }
+};
 const coche = {
   img: null,
   x: 0,
   y: 0,
   width: 0,
   height: 0,
-  inicializar: function () {
+  speedx: 5,
+  inicializar: function() {
     this.img= new Image();
-    this.img.src = "/images/car.png";
-    //this.img.onload = this.mostrar;
+    this.img.src = "./images/car.png";
+    this.img.onload = this.mostrar;
     this.widht = canvas.widht;
     this.height = canvas.height;
 
 
   },
 
-  mostrar: function () {
+  mostrar: function() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
 
   }
 
 
-}
-
-const road = {
-  img: null,
-  x: 0,
-  y: 0,
-  width: 0,
-  height: 0,
-  inicializar: function () {
-    this.img= new Image();
-    this.img.src = "/images/road.png";
-    //this.img.onload = this.mostrar;
-    this.widht = canvas.widht;
-    this.height = canvas.height;
-
-
-  },
-
-  mostrar: function () {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-
-  }
-}
-
-
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
-
-  function startGame() {
-    road.inicializar();
-    road.mostrar();
-    coche.inicializar();
-    coche.mostrar();
-
-
-  }
 };
+
+
+
+function updateGame(){
+  road.mostrar();
+  coche.mostrar();
+
+};
+
+//updateGame();
+//function teclado(evento){
+
+//};
+
+
+ // document.onkeydown = teclado
